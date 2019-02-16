5837e42debe3841c7bea5113f322093d2a9802f6
Ro Flo <iroflood@msn.com>
2019-02-16 04:13:32 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,3 +4,10 @@ def square_array(array)
     new numbers# your code here
 end
 
+def square_array(numbers)
+   # your code here		   new_numbers = []
+ end 		   numbers.each do |num|
+     new_numbers << (num ** 2)
+   end
+   return new_numbers
+ end
